010914dd1b5c41924742bdeb9dfe3668bc6385be
VladaMalysheva <ik02.vlada.m@gmail.com>
2023-01-16 16:02:56 +0300

    Added remaining CRUD operations

--- src/main/java/com/example/springbootresthibernate/rest/EmployeeRestController.java
+++ src/main/java/com/example/springbootresthibernate/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.example.springbootresthibernate.rest;
 import com.example.springbootresthibernate.dao.EmployeeDAO;
 import com.example.springbootresthibernate.entity.Employee;
 import com.example.springbootresthibernate.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,29 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return service.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+        Employee employee = service.findById(id);
+        if(employee==null) throw new RuntimeException("Employee not found");
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public void addEmployee(@RequestBody Employee employee){
+        employee.setId(0);
+        service.saveEmployee(employee);
+    }
+
+    @PutMapping("/employees")
+    public void updateEmployee(@RequestBody Employee employee){
+        service.saveEmployee(employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public void deleteById(@PathVariable int id){
+        if(service.findById(id)==null) throw new RuntimeException("Employee wasn't found");
+        service.deleteById(id);
     }
 }
